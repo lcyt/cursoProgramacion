24696820587fad627756f48e2b92577dd235ba61
lcyt <angel.monsul@gmail.com>
2016-07-29 11:55:53 +0200

    Inicio de rama

---
+++ ContadorBilletes.c
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+#include<stdio.h>
+
+
+//char desglose[6][16];
+
+
+
+void main()
+{
+    int euros[9]={500,200,100,50,20,10,5,2,1};
+    char desglose[6][16];
+    
+    
+    introducirValores(int[] ,char [][]);
+    
+    
+}
---
+++ introducirValores.c
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+#include<stdio.h>
+
+introducirValores(int a[],char b[][])
+{
+
+    
+
+    do
+    {
+        int c;
+        int i = 0;
+        int salir=1;
+        int a=0;
+        for (c=0;c<6;c++)
+        {
+            a=i+c
+            printf("Introduzca un trabajador (para terminar pulse 0):\n");
+            scanf("%s",&desglose[a][0]);
+            if (desglose[a][0]=0)
+            {
+                salir=0;
+                break;
+            }
+            i=i+1;
+            printf("Introduzca el sueldo:\n");
+            scanf("%s",desglose[i+c,0]);
+            comprobarSueldo();
+            i=i+1;
+            desglose[i+c,0]=500;
+            i=i+1;
+            desglose[i+c,0]=200;
+            i=i+1;
+            desglose[i+c,0]=100;
+            i=i+1;
+            desglose[i+c,0]=50;
+            i=i+1;
+        }
+        
+    } while (salir==1);
+    return 0;
+}
