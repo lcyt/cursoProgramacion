764cebcf48e685d592276a3478340e1310c94671
lcyt <angel.monsul@gmail.com>
2016-07-29 13:51:46 +0200

    fichero de prueba, se han convertido a comentarios todos los errores de los otros ficheros para poder probar el fichero prueba. Fichero validarSueldo para ver llamadas a funciones con argumentos

--- prueba.c
+++ prueba.c
@@ -9,16 +9,26 @@
 int main()
 {
  //   char nombre[];
-    int salario;
+    float salario;
     int billete;
+    int entera;
+    int decimal;
     int cantidadEuro;
     
     const int unidadesEuros[9]={500,200,100,50,20,10,5,2,1};
     
     printf("Introducir salario");
-    scanf("%d",&salario);
+    scanf("%.2f",&salario);
+    printf("%f",salario);
     printf("Introducir billete");
-    scanf("%d",&billete);
+ //   scanf("%d",&billete);
+    
+    printf("La parte entera es %d, y la decimal es %d\n\n",entera,decimal);
+    entera=(int)salario;
+    printf("La parte entera es %d, y la decimal es %d\n\n",entera,decimal);
+    decimal=salario*100;
+    
+    printf("La parte entera es %d, y la decimal es %d\n\n",entera,decimal);
     
     int i;
     int posicionBillete;
